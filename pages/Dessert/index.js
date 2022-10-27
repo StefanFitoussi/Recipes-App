d5d71ba0613dbf4315d3c2936379ef5d89225508
Stefan Fitoussi <stefan.fit@web.de>
2022-10-27 11:51:44 +0200

    add veggie and dessert pages

---
+++ pages/Dessert/index.js
@@ -0,0 +1,8 @@
+export default function Dessert {
+    return (
+        <div>
+            <Headline>Welcome to Dessert!</Headline>
+            <p>Welcome to the dessert-page</p>
+        </div>
+    )
+}
---
+++ pages/Veggie/index.js
@@ -0,0 +1,8 @@
+export default function Veggie {
+    return (
+        <div>
+            <Headline>Welcome to Veggie!</Headline>
+            <p>Welcome to the veggie-page</p>
+        </div>
+    )
+}
